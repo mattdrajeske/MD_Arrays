59d8f5df37071ac0019001a240182b216aef6070
Matt <drajeskematt@gmail.com>
2018-10-10 16:54:18 -0600

    add fixes for headers

--- multiply_me.cpp
+++ multiply_me.cpp
@@ -15,31 +15,37 @@ void multiply_me() {
 
 		for (int j = 0; j < COLUMNS; j++) {
 			table[i][j] = (i+1)*(j+1);
-	
 		}
 	}
 
 	//outer loop, prints to console top header
 	for (int i = 0; i < ROWS; i++) {
 		
-		cout << "\t";
+		//cout << "\t";
 
+		//top header
 		if (i == 0) {
-
-			for (int k = 0; k < COLUMNS; k++) { //top header
-				cout << "   " << setw(2) << (k + 1) << ":";
+			cout << "       ";
+			for (int k = 0; k < COLUMNS; k++) {
+				cout << setw(2) << (k + 1) << ":   ";
 			}
 
 		}
 
-		//inner loop, prints to console side header
+		//inner loop, prints to console side header and table
 		for (int j = 0; j < COLUMNS; j++) {
+			
+			
+
+			//ensures format for first row and prints side header
 			if (j == 0) {
-				cout << "\n\n";
+				cout << "\n\n\n" << setw(2) << (i+1) << ":";
 			}
+
+			//table
 			cout << "   " << setw(3) << table[i][j];
 			if (j == (COLUMNS - 1)) {
-				cout << "\n\n";
+				cout << "\n";
 			}
 		}
 	}
